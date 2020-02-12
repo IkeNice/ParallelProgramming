652693768b8db2a90d44362b130e796f4d0c5c9c
IkeNice <49809282+IkeNice@users.noreply.github.com>
2020-02-12 17:03:10 +0300

    add vector by vector method

--- Java/ParallelProgramming/src/all.java
+++ Java/ParallelProgramming/src/all.java
@@ -3,24 +3,34 @@ import java.util.Random;
 import java.util.Vector;
 
 public class all {
-    ArrayList<Integer> myVect = new ArrayList<>();
+    public static int n = 3;
 
+    public static void vectorToNum(int[] vect) {
+        for (int i = 0; i < vect.length; ++i) {
+            System.out.println(vect[i] * n);
+        }
+    }
 
-    public int vectorToNum(ArrayList<Integer> vect) {
-        return 0;
+    public static void vectorToVector(int[] vect1, int[] vect2) {
+        int[] res = new int[vect1.length];
+        for (int i = 0; i < vect1.length; ++i) {
+            res[i] = vect1[i]*vect2[i];
+            System.out.println(res[i]);
+        }
     }
 
     public static void main(String[] args) {
-        int n = 3;
         Random rand = new Random();
         int[] intArray = new int[100];
+        int[] intArray1 = new int[100];
+        int[] intArray2 = new int[100];
         for (int i = 0; i < intArray.length;i++) {
             intArray[i] = rand.nextInt(1000);
+            intArray1[i] = rand.nextInt(1000);
+            intArray2[i] = rand.nextInt(1000);
 //            System.out.println(intArray[i]);
         }
-        for (int i = 0; i < intArray.length; i++) {
-            System.out.println(intArray[i] * n);
-        }
-        System.out.println("test");
+//        vectorToNum(intArray);
+        vectorToVector(intArray1, intArray2);
     }
 }
