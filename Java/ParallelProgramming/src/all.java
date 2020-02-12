39ffd3679d949d37efe557b84e9ce51bc4257d68
IkeNice <49809282+IkeNice@users.noreply.github.com>
2020-02-12 22:27:28 +0300

    add folder

--- Java/ParallelProgramming/src/all.java
+++ Java/ParallelProgramming/src/all.java
@@ -3,34 +3,63 @@ import java.util.Random;
 import java.util.Vector;
 
 public class all {
-    public static int n = 3;
 
-    public static void vectorToNum(int[] vect) {
+    // умножение вектора на число
+    public static void vectorByNum(int[] vect, int n) {
+        int[] res = new int[vect.length];
         for (int i = 0; i < vect.length; ++i) {
-            System.out.println(vect[i] * n);
+            res[i] = vect[i] * n;
+            System.out.println(res[i]);
         }
     }
 
-    public static void vectorToVector(int[] vect1, int[] vect2) {
+    // умножение вектора на вектор
+    public static void vectorByVector(int[] vect1, int[] vect2) {
         int[] res = new int[vect1.length];
         for (int i = 0; i < vect1.length; ++i) {
             res[i] = vect1[i]*vect2[i];
-            System.out.println(res[i]);
+//            System.out.println(res[i]);
+        }
+    }
+
+    // умножение матрицы на число
+    public static void matrByNum(int[][] matr, int n) {
+        int[][] resMatr = new int[matr.length][matr.length];
+        for (int i = 0; i < matr.length; ++i) {
+            for (int j = 0; j < matr.length; ++j) {
+                resMatr[i][j] = matr[i][j] * n;
+                System.out.println(resMatr[i][j]);
+            }
         }
     }
 
     public static void main(String[] args) {
         Random rand = new Random();
+        // массив для умножения вектора на число
         int[] intArray = new int[100];
+        int n = 3;
+        // массивы для умножения вектора на вектор
         int[] intArray1 = new int[100];
         int[] intArray2 = new int[100];
-        for (int i = 0; i < intArray.length;i++) {
+
+        for (int i = 0; i < intArray.length; ++i) {
             intArray[i] = rand.nextInt(1000);
+
             intArray1[i] = rand.nextInt(1000);
             intArray2[i] = rand.nextInt(1000);
-//            System.out.println(intArray[i]);
         }
-//        vectorToNum(intArray);
-        vectorToVector(intArray1, intArray2);
+//        vectorByNum(intArray, n);
+//        vectorByVector(intArray1, intArray2);
+
+        // матрица для умножения на число
+        int[][] intMatr = new int[100][100];
+        int nn = 5;
+        int len = intMatr.length;
+        for (int i = 0; i < len; ++i) {
+            for (int j = 0; j < len; ++j) {
+                intMatr[i][j] = rand.nextInt(1000);
+            }
+        }
+        matrByNum(intMatr, nn);
     }
 }
